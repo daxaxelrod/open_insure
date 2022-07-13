3f54496e5de916edef93d3a5fcba21bcf5b9b3d5
David Axelrod <daxaxelrod@gmail.com>
2022-07-12 23:37:27 -0400

    more form stuff, forms are a lot

--- frontend/src/app/components/dashboard/modals/CreatePolicyModal.tsx
+++ frontend/src/app/components/dashboard/modals/CreatePolicyModal.tsx
@@ -1,5 +1,5 @@
 import React, { Dispatch, SetStateAction, useState } from 'react'
-import { Button, Form, Input, Radio, Modal, Divider, Typography, InputNumber, Select } from 'antd';
+import { Button, Form, Input, Radio, Modal, Divider, Typography, InputNumber, Select, DatePicker, Space, Checkbox } from 'antd';
 
 const { Title, Paragraph } = Typography;
 
@@ -8,19 +8,21 @@ export default function CreatePolicyModal({ isVisible, setIsVisible }: { isVisib
   let createPolicyPending = false;
   const [showAdvancedSettings, setShowAdvancedSettings] = useState(false);
   const [form] = Form.useForm();
-  
+
 
   const handleOk = () => {
-    
-      form
-        .validateFields()
-        .then(values => {
-          // createPod(values);
-          // createPolicy(values);
-        })
-        .catch(info => {
-          console.log('Validate Failed:', info);
-        });
+
+    form
+      .validateFields()
+      .then(values => {
+        // let formattedDate = values['date-picker'].format('YYYY-MM-DD');
+
+        // createPod(values);
+        // createPolicy(values);
+      })
+      .catch(info => {
+        console.log('Validate Failed:', info);
+      });
   }
 
   const handleCancel = () => {
@@ -45,13 +47,6 @@ export default function CreatePolicyModal({ isVisible, setIsVisible }: { isVisib
         form={form}
         layout='vertical'
         onValuesChange={onFormChange}>
-        <Form.Item label="Form Layout" name="layout">
-          <Radio.Group>
-            <Radio.Button value="horizontal">Horizontal</Radio.Button>
-            <Radio.Button value="vertical">Vertical</Radio.Button>
-            <Radio.Button value="inline">Inline</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
         <Form.Item label="Policy Name" name="policyName">
           <Input placeholder="Phone Insurance" />
         </Form.Item>
@@ -61,6 +56,20 @@ export default function CreatePolicyModal({ isVisible, setIsVisible }: { isVisib
             <Select.Option value="renters">Renter's Insurance</Select.Option>
           </Select>
         </Form.Item>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Space size={16}>
+            <Form.Item label="Coverage start date" name="coverage_start_date">
+              <DatePicker format={'MMMM Do, YYYY'} />
+            </Form.Item>
+            <Form.Item label="Coverage length" tooltip="How long coverage lasts from start date" name="coverage_duration" initialValue={12}>
+              <InputNumber min={3} max={36} />
+            </Form.Item>
+          </Space>
+        </div>
+        
+
+        <Divider />
+        <Title level={4}>Advanced Settings</Title>
         <Form.Item label="Governance Type" name="governance_type" initialValue={'direct_democracy'} hidden>
           <Select>
             <Select.Option value="direct_democracy">Direct Democracy</Select.Option>
@@ -72,12 +81,17 @@ export default function CreatePolicyModal({ isVisible, setIsVisible }: { isVisib
             {/* <Select.Option value="perpetual_pool">Capped Pool</Select.Option> */}
           </Select>
         </Form.Item>
-
-        <Divider />
-        <Title level={4}>Advanced Settings</Title>
-        <Form.Item label="Maximum number of policy members" tooltip='Leave blank to allow an unlimited number of people to join.'>
-          <InputNumber min={2}  />
+        <Form.Item label="Maximum number of policy members"
+          name={"max_pod_size"}
+          tooltip='Leave blank to allow an unlimited number of people to join.'>
+          <InputNumber min={2} />
         </Form.Item>
+        <Form.Item label="Allow members to join after policy start"
+          name={"allow_joiners_after_policy_start"}
+          tooltip='Allow people to join the policy after the start date'>
+          <Checkbox />
+        </Form.Item>
+        
       </Form>
     </Modal>
   )
