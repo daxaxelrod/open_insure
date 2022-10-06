725f1616065144a8d8b483ad079740dbb5294b6b
Jack Giunta <jackgiunta@gmail.com>
2022-10-05 21:59:56 -0400

    Update Readme and run prettier on all typescript files in frontend

--- frontend/src/app/components/policies/premiums/UserPremiumsRow.tsx
+++ frontend/src/app/components/policies/premiums/UserPremiumsRow.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function UserPremiumsRow() {
-  return (
-    <div>UserPremiumsRow</div>
-  )
+    return <div>UserPremiumsRow</div>;
 }
--- frontend/src/app/pages/NotFound.tsx
+++ frontend/src/app/pages/NotFound.tsx
@@ -1,15 +1,14 @@
-import { Button } from 'antd'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Button } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
-  return (
-    <div>
-        Url not found!
-        <Link to={'/'}>
-            <Button>Go to home</Button>
-        </Link>
-        
-    </div>
-  )
+    return (
+        <div>
+            Url not found!
+            <Link to={"/"}>
+                <Button>Go to home</Button>
+            </Link>
+        </div>
+    );
 }
