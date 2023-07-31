c8010ee3fc1bc95768c15512a50ca37eeac3fc0f
David Axelrod <daxaxelrod@gmail.com>
2023-07-30 20:05:08 -0400

    hooking up badges

--- gatherer/views.py
+++ gatherer/views.py
@@ -4,6 +4,7 @@ from gatherer.serializers import (
     PolicyLinePropertySerializer,
     PropertyLifeExpectancyGuessSerializer,
 )
+from pods.utils.badges import award_badge
 
 
 class PolicyLinePropertyViewSet(ModelViewSet):
@@ -15,6 +16,13 @@ class PolicyLinePropertyViewSet(ModelViewSet):
 
 class PropertyLifeExpectancyGuessViewSet(ModelViewSet):
     serializer_class = PropertyLifeExpectancyGuessSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+        # award a badge if there is an attached user
+        if self.request.user:
+            award_badge(self.request.user, "Actuarial Contributor")
 
     class Meta:
         model = PropertyLifeExpectancyGuess
